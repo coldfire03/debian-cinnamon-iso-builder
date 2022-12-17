af30de39aaad25a6eecd8fcbc708c1494b3e02fd
Bülent Ergün <93243536+mobilturka@users.noreply.github.com>
2022-12-17 12:35:49 +0300

    Update customize-airootfs.sh

--- muslim/customize-airootfs.sh
+++ muslim/customize-airootfs.sh
@@ -6,6 +6,10 @@ apt install wget
 wget https://github.com/mobilturka/17g-installer/raw/master/17g-installer_1.0_all.deb
 apt install ./*.deb -yq --allow-downgrades
 
+## qmplay
+wget http://depo.pardus.org.tr/pardus/pool/contrib/q/qmplay2/qmplay2_22.10.23-1~par21_amd64.deb
+apt install ./qmplay2_22.10.23-1~par21_amd64.deb -y
+
 #### Disable recommends by default
 cat > /etc/apt/apt.conf.d/01norecommend << EOF
 APT::Install-Recommends "0";
