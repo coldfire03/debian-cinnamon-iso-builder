a36d7e76aac97d3597a5ea913763e69ba740ac9f
sulincix <parduscix@yandex.ru>
2022-12-27 17:11:39 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Muslim Linux iso build profile.
-git clone https://github.com/muslimos/teaiso
+git clone https://gitlab.com/tearch-linux/applications-and-tools/teaiso
 cd teaiso
 sudo apt update
 sudo apt install xorriso grub-pc-bin grub-efi mtools make python3 dosfstools e2fsprogs squashfs-tools python3-yaml gcc wget curl unzip xz-utils zstd debootstrap -y
