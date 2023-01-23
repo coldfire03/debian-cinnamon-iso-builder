bb63f55539ffa1bcd9d0153e121a8a0b626e9c78
coldfire <aligolpinar2005@gmail.com>
2023-01-23 23:15:21 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 #git clone https://gitlab.com/tearch-linux/applications-and-tools/teaiso
 =======
 >>>>>>> Stashed changes:teaiso/build.sh
-git clone https://github.com/coldfire03/teaisol
-cd teaisol
+git clone https://github.com/coldfire03/teaiso
+cd teaiso
 sudo apt update
 sudo apt install xorriso grub-pc-bin grub-efi mtools make python3 dosfstools e2fsprogs squashfs-tools python3-yaml gcc wget curl unzip xz-utils zstd debootstrap -y
 make && make install
